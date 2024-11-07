d58dbd81b0b1484286aae62ed11db94cead7e25e
AjCruzat89 <ajcruzat89@gmail.com>
2024-11-08 02:05:01 +0800

    :D

--- server.js
+++ server.js
@@ -12,12 +12,6 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: false
 }));
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //<!--===============================================================================================-->
