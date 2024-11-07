d5c45ab7c53bb71a59b84cfda739011248b477ee
AjCruzat89 <ajcruzat89@gmail.com>
2024-11-08 01:47:05 +0800

    :D

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ app.use(cors({
     origin: 'http://localhost:5173', 
     methods: ['GET', 'POST', 'OPTIONS'],  
     allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true 
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
