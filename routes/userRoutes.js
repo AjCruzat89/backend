35eaa806942793189341ae89be85786fda2f4623
AjCruzat89 <ajcruzat89@gmail.com>
2024-11-08 02:04:38 +0800

    :D

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,12 +3,6 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 //<!--===============================================================================================-->
-router.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
 router.post('/register', userController.register);
 router.post('/login', userController.login);
 router.post('/admin-auth', userController.adminAuth);
