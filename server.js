d2ffa80e5d2982ca7605def3cd80ac89a724743d
AjCruzat89 <ajcruzat89@gmail.com>
2024-11-08 00:15:43 +0800

    :D

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const cors = require('cors');
 const PORT = 3000;
 //<!--===============================================================================================-->
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //<!--===============================================================================================-->
